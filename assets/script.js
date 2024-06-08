a42e6fbe004680ab2d5cdf88ed9a5bd4c1438ad7
Leena <leenacruz@gmail.com>
2024-06-08 15:07:53 -0700

    Nested Fetch works

--- assets/script.js
+++ assets/script.js
@@ -10,12 +10,12 @@ const APIKey = '4527ed5e22cd18768af91f242a69b9a2';
 
 function getCoords() {
   const city = ciudadInput.value;
-  fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=3&appid=${APIKey}`)
+  fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      // console.log(data);
+      console.log(data);
       const coordinates = {
         name: data[0].name,
         lat: data[0].lat,
@@ -23,15 +23,21 @@ function getCoords() {
       }
       // console.log(coordinates)\
       // coords = localStorage.setItem("coordinates", JSON.stringify(coordinates));
-      let cityList = JSON.parse(localStorage.getItem("coordinates")) || [];
+      // Creates and array of coordinates
+      const cityList = JSON.parse(localStorage.getItem("coordinates")) || [];
       cityList.push(coordinates);
       localStorage.setItem("coordinates", JSON.stringify(cityList));
-      // console.log(cityList);
+
+
+
+      // localStorage.setItem("coordinates", JSON.stringify(coordinates));
+
+      console.log(cityList);
     });
 
-// return cityList;
-getWeather()
- 
+  // return cityList;
+  getWeather()
+
 }
 
 
@@ -40,18 +46,24 @@ function getWeather() {
 
   // getCoords();
   const coords = JSON.parse(localStorage.getItem("coordinates"));
-  // console.log (coords);
+  console.log(coords);
 
   // esta agarrando el ultimo registro de local Storage, quiero que agarre el del input
-const number = coords.length;
+  // const number = coords.length;
   // let coords = getCoords();
-  const cityData = coords[number-1];
+  // const cityData = coords[number-1];
   // console.log(typeof cityData + " " + cityData);
   // console.log(cityData);
-  const lat = cityData.lat;
-  const lon = cityData.lon;
-  // console.log(typeof lat + " " + lat);
-  // console.log(typeof lon + " " + lon);
+  // const lat = JSON.stringify(coords.lat);
+  // const lon = JSON.stringify(coords.lon);
+
+  const newCity = coords.length - 1;
+  console.log(typeof newCity + " " + newCity);
+
+  const lat = coords[newCity].lat;
+  const lon = coords[newCity].lon;
+  console.log(typeof lat + " " + lat);
+  console.log(typeof lon + " " + lon);
   fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`)
     .then(function (response) {
       return response.json();
@@ -59,28 +71,142 @@ const number = coords.length;
     .then(function (data) {
       console.log(data);
 
-      const weather = {
-        name: data.city.name,
-        temp: data.list[0].main.temp,
-        wind: data.list[0].wind.speed,
-        lat: data.city.coord.lat,
-      }
+      for (let i = 0; i < data.length; i++) {
+        const weather = {
+          name: data.city.name,
+          date: data.list[i].dt_txt,
+          temp: data.list[i].main.temp,
+          wind: data.list[i].wind.speed,
+          lat: data.city.coord.lat,
 
-      console.log (weather);
+        }
+
+        const cityForecast = JSON.parse(localStorage.getItem("forecast")) || [];
+        cityForecast.push(weather);
+        localStorage.setItem("forecast", JSON.stringify(cityForecast));
+        // console.log(weather);
+      }
+    });
+  // createCard();
+  searchInput.value = "";
+
+}
+
+
+
+
+function getWeatherGOD() {
+
+
+  const city = ciudadInput.value;
+  fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      const coordinates = {
+        name: data[0].name,
+        lat: data[0].lat,
+        lon: data[0].lon,
+      }
+      // console.log(coordinates)\
+      // coords = localStorage.setItem("coordinates", JSON.stringify(coordinates));
+      // Creates and array of coordinates
+      const cityList = JSON.parse(localStorage.getItem("coordinates")) || [];
+      cityList.push(coordinates);
+      localStorage.setItem("coordinates", JSON.stringify(cityList));
+      // localStorage.setItem("coordinates", JSON.stringify(coordinates));
+      console.log(cityList);
+      return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+
+          for (let i = 0; i < data.length; i++) {
+            const weather = {
+              name: data.city.name,
+              date: data.list[i].dt_txt,
+              temp: data.list[i].main.temp,
+              wind: data.list[i].wind.speed,
+              lat: data.city.coord.lat,
+            }
+
+            const cityForecast = JSON.parse(localStorage.getItem("forecast")) || [];
+            cityForecast.push(weather);
+            localStorage.setItem("forecast", JSON.stringify(cityForecast));
+            // console.log(weather);
+          }
+
+        });
+      // createCard();
 
     });
   }
-    // createCard();
-    searchInput.value = "";
 
 
-// function createCard (weather) {
-// const weatherCard = $('<div>');
-// const cityNameEl = $('<h2>').textcontent(weather.name)
-// weatherCard.append(cityNameEl)
+  function createCard(weather) {
+    const weatherCard = $('<div>');
+    const cityNameEl = $('<h2>').textcontent(weather.name)
+    weatherCard.append(cityNameEl)
 
 
-// }
+  }
 
 
-searchBtn.addEventListener('click', getCoords);
+  // funciona con otro API :(
+
+  // function getWeather() {
+
+
+  //   const city = ciudadInput.value;
+  //   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=40&appid=${APIKey}`)
+  //     .then(function (response) {
+  //       return response.json();
+  //     })
+  //     .then(function (data) {
+  //       console.log(data);
+
+  //       // Luego hago esto para mostrar solo el tiempo
+  //       const time = data.list[0].dt_txt.split(" ").pop();
+
+  //       console.log(time)
+  //       // Recuerda hacer split a la fecha  split (" "). slice(0,-1) cuando crees cartas
+
+  //       for (let i = 0; i < data.list.length; i++) {
+
+  //         if (data.list[i].dt_txt.includes(time)) {
+
+  //           const weather = {
+  //             name: data.city.name,
+  //             date: data.list[i].dt_txt,
+  //             temp: data.list[i].main.temp,
+  //             wind: data.list[i].wind.speed,
+  //             lat: data.city.coord.lat,
+  //           }
+
+  //           const cityForecast = JSON.parse(localStorage.getItem("forecast")) || [];
+  //           cityForecast.push(weather);
+  //           localStorage.setItem("forecast", JSON.stringify(cityForecast));
+
+  //         }
+
+  //       }
+  //     });
+  //   // createCard();
+  //   searchInput.value = "";
+  // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  searchBtn.addEventListener('click', getWeatherGOD);
